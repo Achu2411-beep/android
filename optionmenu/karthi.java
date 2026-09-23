92a7a67aa2297d2e43e519c0ad912f22b46fbbc0
Aswathivijayachandran <aswathivijayachandran@gmail.com>
2026-09-23 15:09:19 +0530

    Add Karthi activity with options menu

---
+++ optionmenu/karthi.java
@@ -0,0 +1,54 @@
+package com.example.optionmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Karthi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karthi);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_home) {
+            Intent intent = new Intent(Karthi.this, Homeactivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menu_about) {
+            Intent intent = new Intent(Karthi.this, Aboutactivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menu_vijay) {
+            Intent intent = new Intent(Karthi.this, VijayActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menu_karthi) {
+            Intent intent = new Intent(Karthi.this, Karthi.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
